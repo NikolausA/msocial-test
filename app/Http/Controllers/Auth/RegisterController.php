d85a6434c169f94b4c04fe9ab6eccdf2d57005bc
NikolausA <nik.aleksenko@gmail.com>
2022-02-09 21:01:36 +0300

    registration is implemented

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Http\Requests\SignInRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function signUp(Request $request) 
+    public function signUp(SignInRequest $request) 
     {
-        
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return response()->json($user, 200);
     }
 }
